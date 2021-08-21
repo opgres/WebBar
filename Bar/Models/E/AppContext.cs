eb795fc9bc1e827424fa1a27a79c0ff1eae47bbc
Sergey <sergpoka@rambler.ru>
2021-08-21 21:40:04 +0300

    Добавьте файлы проекта.

---
+++ Bar/Models/E/AppContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bar.Models.E
+{
+    public class AppContext : DbContext
+    {
+        public AppContext(DbContextOptions<AppContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+        public DbSet<Ingredient> Ingredients { get; set; }
+    }
+}
